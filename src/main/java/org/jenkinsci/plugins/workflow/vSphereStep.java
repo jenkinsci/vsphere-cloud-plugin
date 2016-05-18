3fafeffb859e8d3209d65eaa1c049289335167d6
witokondoria <witokondoria@gmail.com>
2016-05-18 12:56:09 +0200

    reuse vSphereBuildStep jellies

--- src/main/java/org/jenkinsci/plugins/workflow/vSphereStep.java
+++ src/main/java/org/jenkinsci/plugins/workflow/vSphereStep.java
@@ -1,18 +1,16 @@
 package org.jenkinsci.plugins.workflow;
 
 import com.google.inject.Inject;
-import hudson.EnvVars;
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Launcher;
+import hudson.*;
+import hudson.model.Hudson;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import hudson.slaves.Cloud;
+import hudson.util.ListBoxModel;
+import org.jenkinsci.plugins.vSphereCloud;
 import org.jenkinsci.plugins.vsphere.VSphereBuildStep;
 import org.jenkinsci.plugins.vsphere.VSphereBuildStepContainer;
-import org.jenkinsci.plugins.vsphere.builders.Clone;
-import org.jenkinsci.plugins.vsphere.builders.Deploy;
-import org.jenkinsci.plugins.vsphere.builders.PowerOff;
-import org.jenkinsci.plugins.vsphere.builders.PowerOn;
+import org.jenkinsci.plugins.vsphere.builders.*;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepExecution;
@@ -25,50 +23,16 @@ import org.kohsuke.stapler.DataBoundSetter;
  */
 public class vSphereStep extends AbstractStepImpl {
 
-    private String action;
     private String serverName;
-    private String vm;
-    private String template;
-    private String clone;
-    private boolean linkedClone;
-    private String cluster;
-    private String datastore;
-    private boolean powerOn;
 
-    public String getAction() {
-        return action;
-    }
+    private VSphereBuildStep buildStep;
 
     public String getServerName() {
         return serverName;
     }
 
-    public String getVm() {
-        return vm;
-    }
-
-    public String getTemplate() {
-        return template;
-    }
-
-    public String getClone() {
-        return clone;
-    }
-
-    public boolean isLinkedClone() {
-        return linkedClone;
-    }
-
-    public String getCluster() {
-        return cluster;
-    }
-
-    public String getDatastore() {
-        return datastore;
-    }
-
-    public boolean isPowerOn() {
-        return powerOn;
+    public VSphereBuildStep getBuildStep() {
+        return buildStep;
     }
 
     @DataBoundConstructor
@@ -76,48 +40,13 @@ public class vSphereStep extends AbstractStepImpl {
     }
 
     @DataBoundSetter
-    public void setAction(String action) {
-        this.action = action;
+    public void setBuildStep(VSphereBuildStep buildStep) {
+        this.buildStep = buildStep;
     }
 
     @DataBoundSetter
     public void setServerName(String serverName) {
         this.serverName = serverName;
-    }
-
-    @DataBoundSetter
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @DataBoundSetter
-    public void setClone(String clone) {
-        this.clone = clone;
-    }
-
-    @DataBoundSetter
-    public void setLinkedClone(Boolean linkedClone) {
-        this.linkedClone = linkedClone;
-    }
-
-    @DataBoundSetter
-    public void setCluster(String cluster) {
-        this.cluster = cluster;
-    }
-
-    @DataBoundSetter
-    public void setDatastore(String datastore){
-        this.datastore = datastore;
-    }
-
-    @DataBoundSetter
-    public void setPowerOn(Boolean powerOn) {
-        this.powerOn = powerOn;
-    }
-
-    @DataBoundSetter
-    public void setVm(String vm) {
-        this.vm = vm;
     }
 
     @Extension
@@ -134,16 +63,35 @@ public class vSphereStep extends AbstractStepImpl {
 
         @Override
         public String getDisplayName() {
-            return "Invoke an vSphere action (deploy, powerOff, powerOn or clone)";
+            return "Invoke an vSphere action, exposing the VM IP under some actions";
         }
 
+        public ListBoxModel doFillServerNameItems() {
+            ListBoxModel select = new ListBoxModel();
+
+            try {
+                boolean hasVsphereClouds = false;
+                for (Cloud cloud : Hudson.getInstance().clouds) {
+                    if (cloud instanceof vSphereCloud) {
+                        hasVsphereClouds = true;
+                        select.add(((vSphereCloud) cloud).getVsDescription());
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return select;
+        }
+
+        public DescriptorExtensionList<VSphereBuildStep, VSphereBuildStep.VSphereBuildStepDescriptor> getBuildSteps() {
+            return Hudson.getInstance().getDescriptorList(VSphereBuildStep.class);
+        }
     }
 
     public static final class vSphereExecution extends AbstractSynchronousNonBlockingStepExecution<String> {
 
         private static final long serialVersionUID = 1;
-
-        private transient VSphereBuildStep action;
 
         private transient VSphereBuildStepContainer vSphereBSC;
 
@@ -167,35 +115,16 @@ public class vSphereStep extends AbstractStepImpl {
 
         @Override
         protected String run() throws Exception {
-            System.out.println(step.getAction());
-
             String IP = "";
 
-            switch (step.getAction().toLowerCase()) {
-                case "poweron":
-                    action = new PowerOn(step.getVm(), 10);
-                    break;
-                case "poweroff":
-                    action = new PowerOff(step.getVm(), true, true);
-                    break;
-                case "deploy":
-                    action = new Deploy(step.getTemplate(), step.getClone(), step.isLinkedClone(), "", step.getCluster(), step.getDatastore(), step.isPowerOn());
-                    break;
-                case "clone":
-                    action = new Clone(step.getTemplate(), step.getClone(), step.isLinkedClone(), "", step.getCluster(), step.getDatastore(), step.isPowerOn());
-                    break;
-                default:
-                    break;
-            }
-            vSphereBSC = new VSphereBuildStepContainer(action, step.getServerName());
+            vSphereBSC = new VSphereBuildStepContainer(step.getBuildStep(), step.getServerName());
             vSphereBSC.perform(run, filePath, launcher, listener);
-            if ("poweron".equals(step.getAction().toLowerCase()) ||
-                    "deploy".equals(step.getAction().toLowerCase()) ||
-                    "clone".equals(step.getAction().toLowerCase())) {
-                IP = action.getIP();
+            if (step.getBuildStep().getClass().toString().contains("PowerOn") ||
+                    step.getBuildStep().getClass().toString().contains("Deploy") ||
+                    step.getBuildStep().getClass().toString().contains("Clone")) {
+                IP = step.getBuildStep().getIP();
                 envVars.put("VSPHERE_IP", IP);
             }
-            action = null;
             vSphereBSC = null;
 
             return IP;
